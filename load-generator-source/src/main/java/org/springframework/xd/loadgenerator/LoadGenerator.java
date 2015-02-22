a255b0d32adca286736c847904aca4f74917667d
Glenn Renfro <grenfro@pivotal.io>
2015-02-22 17:26:49 -0500

    Updated based on code review.

--- load-generator-source/src/main/java/org/springframework/xd/loadgenerator/LoadGenerator.java
+++ load-generator-source/src/main/java/org/springframework/xd/loadgenerator/LoadGenerator.java
@@ -23,19 +23,24 @@ import org.springframework.integration.support.MessageBuilder;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Is a source module that generates a series of fixed size messages to be 
+ * Is a source module that generates a series of fixed size messages to be
  * dispatched to a stream.  The load-generator is used to test the performance of
  * XD in different environments.
  *
  * @author Glenn Renfro
  */
-public class LoadGenerator extends MessageProducerSupport implements Runnable {
+public class LoadGenerator extends MessageProducerSupport {
 
 	private int producers;
 	private int messageSize;
 	private int messageCount;
+	private final AtomicBoolean running = new AtomicBoolean(false);
+	private ExecutorService executorService;
 
 	Logger logger = LoggerFactory.getLogger(LoadGenerator.class);
 
@@ -47,50 +52,62 @@ public class LoadGenerator extends MessageProducerSupport implements Runnable {
 
 	@Override
 	protected void doStart() {
-		for (int x = 0; x < producers; x++) {
-			Thread t = new Thread(this);
-			t.start();
-		}
-	}
-
-	private void send() {
-		logger.info("Sending " + messageCount +" messages");
-		for (int x = 0; x < messageCount; x++) {
-			sendMessage(MessageBuilder.withPayload(createMessage(x)).build());
-		}
-		logger.info("All Messages Dispatched");
-	}
-
-	/**
-	 * Creates a message that can be consumed by the Rabbit perfTest Client
-	 *
-	 * @param sequenceNumber a number to be prepended to the message
-	 * @return a byte array containing a series of numbers that match the message size as
-	 * specified by the messageSize constructor arg.
-	 */
-	private byte[] createMessage(int sequenceNumber) {
-		byte message[] = new byte[messageSize];
-		try {
-			ByteArrayOutputStream acc = new ByteArrayOutputStream();
-			DataOutputStream d = new DataOutputStream(acc);
-			long nano = System.nanoTime();
-			d.writeInt(sequenceNumber);
-			d.writeLong(nano);
-			d.flush();
-			acc.flush();
-			byte[] m = acc.toByteArray();
-			if (m.length <= messageSize) {
-				System.arraycopy(m, 0, message, 0, m.length);
-				return message;
-			} else {
-				return m;
+		executorService = Executors.newFixedThreadPool(producers);
+		if (running.compareAndSet(false, true)) {
+			for (int x = 0; x < producers; x++) {
+				executorService.submit(new Producer());
 			}
-		} catch (IOException ioe) {
-			throw new IllegalStateException(ioe);
 		}
 	}
 
-	public void run() {
-		send();
+	@Override
+	protected void doStop() {
+		if (running.compareAndSet(true, false)) {
+			executorService.shutdown();
+		}
+	}
+
+	protected class Producer implements Runnable {
+		
+		private void send() {
+			logger.info("Sending " + messageCount + " messages");
+			for (int x = 0; x < messageCount; x++) {
+				sendMessage(MessageBuilder.withPayload(createMessage(x)).build());
+			}
+			logger.info("All Messages Dispatched");
+		}
+
+		/**
+		 * Creates a message that can be consumed by the Rabbit perfTest Client
+		 *
+		 * @param sequenceNumber a number to be prepended to the message
+		 * @return a byte array containing a series of numbers that match the message size as
+		 * specified by the messageSize constructor arg.
+		 */
+		private byte[] createMessage(int sequenceNumber) {
+			byte message[] = new byte[messageSize];
+			try {
+				ByteArrayOutputStream acc = new ByteArrayOutputStream();
+				DataOutputStream d = new DataOutputStream(acc);
+				long nano = System.nanoTime();
+				d.writeInt(sequenceNumber);
+				d.writeLong(nano);
+				d.flush();
+				acc.flush();
+				byte[] m = acc.toByteArray();
+				if (m.length <= messageSize) {
+					System.arraycopy(m, 0, message, 0, m.length);
+					return message;
+				} else {
+					return m;
+				}
+			} catch (IOException ioe) {
+				throw new IllegalStateException(ioe);
+			}
+		}
+
+		public void run() {
+			send();
+		}
 	}
 }
