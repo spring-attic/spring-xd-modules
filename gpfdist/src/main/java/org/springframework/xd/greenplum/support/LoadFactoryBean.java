1de2ad083feba39a5e9696a3809c4899d3396734
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-04-02 14:44:06 +0100

    Initial implementation of gpfdist sink

---
+++ gpfdist/src/main/java/org/springframework/xd/greenplum/support/LoadFactoryBean.java
@@ -0,0 +1,62 @@
+package org.springframework.xd.greenplum.support;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.Assert;
+
+/**
+ * FactoryBean for easy creation and configuration of {@link GreenplumLoad}
+ * instances.
+ *
+ * @author Janne Valkealahti
+ *
+ */
+public class LoadFactoryBean implements FactoryBean<GreenplumLoad>, InitializingBean, DisposableBean {
+
+	private DataSource dataSource;
+
+	private LoadConfiguration loadConfiguration;
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public GreenplumLoad getObject() throws Exception {
+		return new DefaultGreenplumLoad(loadConfiguration, new DefaultLoadService(jdbcTemplate));
+	}
+
+	@Override
+	public Class<GreenplumLoad> getObjectType() {
+		return GreenplumLoad.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws IOException {
+		Assert.notNull(dataSource, "DataSource must not be null.");
+		Assert.notNull(loadConfiguration, "Load configuration must not be null.");
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void setLoadConfiguration(LoadConfiguration LoadConfiguration) {
+		this.loadConfiguration = LoadConfiguration;
+	}
+
+}
