9c9f62c705ae7ede0b52049151c9cf056f1342e8
Muhammad Ali <ali.iqbal@gmail.com>
2015-05-08 03:20:09 +0400

    updating licensing to Apache license

--- xslt-transformer/src/main/java/com/acme/XsltTransformerModuleConfig.java
+++ xslt-transformer/src/main/java/com/acme/XsltTransformerModuleConfig.java
@@ -1,12 +1,16 @@
 /*
+ * Copyright 2015 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright (c) 2015 Pivotal Software, Inc. All Rights Reserved
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This software contains the intellectual property of Pivotal Software, Inc. or is
- * licensed to Pivotal Software, Inc. from third parties. Use of this software and
- * the intellectual property contained therein is expressly limited to the
- * terms and conditions of the License Agreement under which it is provided
- * by or on behalf of Pivotal Software, Inc.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.acme;
